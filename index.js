59503e36ca901f03d1033eccfb0e531aa0e9cd2f
evilpython <a.rebeka@protonmail.com>
2024-09-30 13:25:44 +0700

    step01

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-"use strict";
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов уже посмотрели?', '');
+
+const personaMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', '');
+const b = prompt('на сколько оцените его?', '');
+const c = prompt('Один из последних просмотренных фильмов', '');
+const d = prompt('на сколько оцените его?', '');
+
+personaMovieDB.movies[a] = b;
+personaMovieDB.movies[c] = d;
+
+console.log(personaMovieDB);
